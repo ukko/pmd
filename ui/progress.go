59894fe2049fe2afe5f1d38f2ae60ab7a5f13538
Max Kamashev <kamashev@gollard.ru>
2014-12-25 00:26:43 +0300

    Started workin on its own timer

--- ui/progress.go
+++ ui/progress.go
@@ -1,13 +1,40 @@
 package ui
 
-import (
+import "time"
+
 //"github.com/nsf/termbox-go"
+
+const (
+	STATE_RUNNING = iota
+	STATE_BREAKED
+	STATE_FINISHED
+	STATE_UNKNOWN
 )
 
 type Progress struct {
-	X, Y int
+	Title      string
+	Duration   time.Duration
+	State      byte
+	StartTime  time.Time
+	FinishTime time.Time
+	progress   chan int
 }
 
-func (p *Progress) New() bool {
+// Start timer
+func (p *Progress) Start() error {
+	p.StartTime = time.Now()
+	p.FinishTime = p.StartTime.Add(p.Duration)
+	p.State = STATE_RUNNING
 
+	return nil
+}
+
+// Stop timer
+func (p *Progress) Stop() error {
+	return nil
+}
+
+type ProgressWidget struct {
+	Progress
+	X, Y, W, H int
 }
